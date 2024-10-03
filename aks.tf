37f86df4f5aab984f1852835cb3f5e21bb8cf77e
nishuri <mn.manishcandy268@gmail.com>
2024-10-03 16:02:45 +0200

    Update aks.tf

--- aks.tf
+++ aks.tf
@@ -1,13 +1,22 @@
-resource "azurerm_kubernetes_cluster" "main" {
-  node_os_channel_upgrade             = "NodeImage"
+resource "azurerm_kubernetes_cluster" "example" {
 
-  maintenance_window_node_os {
-  frequency = "Weekly"
-  interval  = 1
-  day_of_week = "Monday"
-  start_time  = "00:00"
-  start_date  = "2024-10-07T00:00:00Z"
-  utc_offset  = "+00:00"
-  duration = 6
-}
+  dynamic "node_os_channel_upgrade" {
+    for_each = var.environment == "dev" ?  : []
+    content {
+      node_os_channel_upgrade = "NodeImage"
+    }
+  }
+
+  dynamic "maintenance_window_node_os" {
+    for_each = var.environment == "dev" ?  : []
+    content {
+      frequency   = "Weekly"
+      interval    = 1
+      day_of_week = "Monday"
+      start_time  = "00:00"
+      start_date  = "2024-10-07T00:00:00Z"
+      utc_offset  = "+00:00"
+      duration    = 6
+    }
+  }
 }
