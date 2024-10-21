d0d147e274aea208260183848be427f52786e5b5
nishuri <mn.manishcandy268@gmail.com>
2024-10-21 20:28:25 +0200

    Update cloudfrontdistrubution.tf

--- cloudfrontdistrubution.tf
+++ cloudfrontdistrubution.tf
@@ -1,56 +1,22 @@
-
 module "cdn" {
   source = "https://github.com/nishuri/TF-Modules/blob/main/CloudFront"
 
-  aliases = ["cdn.example.com" , "anothercdn.example.com"]
+  aliases             = var.aliases
+  comment             = var.comment
+  enabled             = var.enabled
+  is_ipv6_enabled     = var.is_ipv6_enabled
+  price_class         = var.price_class
+  retain_on_delete    = var.retain_on_delete
+  wait_for_deployment = var.wait_for_deployment
+  web_acl_id          = var.web_acl_id
 
-  comment             = "My cloudfrount project"
-  enabled             = true
-  is_ipv6_enabled     = false
-  price_class         = "PriceClass_100"
-  retain_on_delete    = false
-  wait_for_deployment = false
-  web_acl_id          = aws_wafv2_web_acl.example.arn
+  logging_config = var.logging_config
 
+  origin = var.origin
 
-  logging_config = {
-    bucket = "logs-my-cdn.s3.amazonaws.com"
-  }
+  default_cache_behavior = var.default_cache_behavior
 
-  origin = {
-    something = {
-      domain_name = "something.example.com"
-      custom_origin_config = {
-        http_port              = 80
-        https_port             = 443
-        origin_protocol_policy = "match-viewer"
-        origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
-      }
-    }
+  viewer_certificate = var.viewer_certificate
 
-  default_cache_behavior = {
-    target_origin_id       = "something"
-    viewer_protocol_policy = "redirect-to-https"
-
-    allowed_methods = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods  = ["GET", "HEAD"]
-    compress        = true
-    default_ttl     = 3600
-    max_ttl         = 86400
-    min_ttl         = 0
-    cache_policy_id = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad"
-    origin_request_policy_id = "216a2d-6df8-44a3-9df3-4b5a84be39ad"
-  }
-
-  viewer_certificate = {
-    acm_certificate_arn = "arn:aws:acm:us-east-1:135367859851:certificate/1032b155-22da-4ae0-9f69-e206f825458b"
-    ssl_support_method  = "sni-only"
-    minimum_protocol_version = "TLSv1.2_2021"
-  }
-
-  tags = {
-    Environment = "Production"
-    Project     = "MyProject"
-    Owner       = "YourName"
-  }
+  tags = var.tags
 }
