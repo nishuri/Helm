cf38875d1c5347a6b739e91f69e9d698c0122fde
nishuri <mn.manishcandy268@gmail.com>
2025-03-21 12:19:06 +0100

    Update webapp.ps1

--- Azure/Automation/webapp.ps1
+++ Azure/Automation/webapp.ps1
@@ -4,16 +4,17 @@ param (
         [Parameter(Mandatory=$true)]
         [string]$resourceGroupName,
         [Parameter(Mandatory=$true)]
-        [string]$webAppName
+        [string[]]$webAppNames
     )
 
     # Setting your Azure subscription
     az account set --subscription $subscriptionId
 
-    # Getting the status of the webapp
+    foreach ($webAppName in $webAppNames) {
+    # Get the status of the web app
     $webAppStatus = az webapp show --name $webAppName --resource-group $resourceGroupName --query "state" --output tsv
 
-    # Check the status of the webapp and start or stop the app accordingly
+    # Check the status and start or stop the web app accordingly
     if ($webAppStatus -eq "Running") {
         az webapp stop --name $webAppName --resource-group $resourceGroupName
         Write-Output "Azure Web App $webAppName stopped successfully."
@@ -23,6 +24,7 @@ param (
     } else {
         Write-Output "Azure Web App $webAppName is in an unknown state: $webAppStatus"
     }
+}
 
 # az functionapp show --name MyFunctionApp --resource-group MyResourceGroup
 # az functionapp start --name MyFunctionApp --resource-group MyResourceGroup
