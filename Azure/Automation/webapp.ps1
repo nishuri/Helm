a6819be2da3e6201fc2643e35c254655645eea4b
nishuri <mn.manishcandy268@gmail.com>
2025-03-24 17:06:47 +0100

    Update webapp.ps1

--- Azure/Automation/webapp.ps1
+++ Azure/Automation/webapp.ps1
@@ -12,19 +12,30 @@ param (
     # Setting your Azure subscription
     az account set --subscription $subscriptionId
 
-    foreach ($webAppName in $webAppNames) {
+    # Get the current hour
+    $currentHour = (Get-Date).Hour
+
+foreach ($webAppName in $webAppNames) {
     # Get the status of the web app
     $webAppStatus = az webapp show --name $webAppName --resource-group $resourceGroupName --query "state" --output tsv
 
-    # Check the status and start or stop the web app accordingly
-    if ($webAppStatus -eq "Running") {
-        az webapp stop --name $webAppName --resource-group $resourceGroupName
-        Write-Output "Azure Web App $webAppName stopped successfully."
-    } elseif ($webAppStatus -eq "Stopped") {
-        az webapp start --name $webAppName --resource-group $resourceGroupName
-        Write-Output "Azure Web App $webAppName started successfully."
+    if ($currentHour -eq 7) {
+        if ($webAppStatus -eq "Stopped") {
+            az webapp start --name $webAppName --resource-group $resourceGroupName
+            Write-Output "Azure Web App $webAppName started successfully."
+        } else {
+            Write-Output "Azure Web App $webAppName is already running."
+        }
+    } elseif ($currentHour -eq 19) {
+        # Check the status at 7 PM
+        if ($webAppStatus -eq "Running") {
+            az webapp stop --name $webAppName --resource-group $resourceGroupName
+            Write-Output "Azure Web App $webAppName stopped successfully."
+        } else {
+            Write-Output "Azure Web App $webAppName is already stopped."
+        }
     } else {
-        Write-Output "Azure Web App $webAppName is in an unknown state: $webAppStatus"
+        Write-Output "Current time is not 7 AM or 7 PM."
     }
 }
 
