53248f5d2b09afbfcbcd6b042603254869b1eb65
nishuri <mn.manishcandy268@gmail.com>
2025-04-08 20:35:57 +0530

    Update movefiles.ps1

--- Powershell/movefiles.ps1
+++ Powershell/movefiles.ps1
@@ -2,6 +2,7 @@ $srcPath = "C:\ServiceNow MID Server Azure_MID\agent\export\bcm"
 $destPath = "Z:\"
 
 robocopy $srcPath $destPath /MOVE /E
+New-Item -Path 'C:\ServiceNow MID Server Azure_MID\agent\export' -Name 'bcm' -ItemType Directory
 
 if ($LASTEXITCODE -ge 8) {
     Write-Error "Robocopy encountered an error with exit code $LASTEXITCODE"
